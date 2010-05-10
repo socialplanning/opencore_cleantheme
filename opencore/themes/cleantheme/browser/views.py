fff467b8ec449e607a1f51be2c78cb22ca8d2a10
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-05-10 21:10:09 +0000

    Hook in "Doors" infrastructure for swapping out theme based on site domain  * Register a TALES namespace opencore_doors  * Add static_root utility to that namespace which dispatches to a subdirectory of /++static++/ based on the original domain  * Remove logo files which now live in the directory served by Paste#static at /++static++/open.indaba-network.net

--- opencore/themes/cleantheme/browser/views.py
+++ opencore/themes/cleantheme/browser/views.py
@@ -1,5 +1,7 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.Five.viewlet.viewlet import ViewletBase
 from opencore.nui.main.view import GetStarted as GetStartedBase
 
 class GetStarted(GetStartedBase):
     render = ViewPageTemplateFile('get-started.pt')
+
---
+++ opencore/themes/cleantheme/utils.py
@@ -0,0 +1,17 @@
+from zope import component
+from zope import interface
+from zope.traversing.interfaces import IPathAdapter
+
+def static_root_for_domain(environ):
+    domain = environ['HTTP_X_FORWARDED_SERVER']
+    return "/++static++/%s" % domain
+
+class Tales(object):
+    component.adapts(interface.Interface)
+    interface.implements(IPathAdapter)
+
+    def __init__(self, context):
+        self.request = context.request
+
+    def static_root(self):
+        return static_root_for_domain(self.request.environ)
