f5249fc96725dcd5de8769da8bcb230100cfa91c
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-06-06 20:57:51 +0000

    plug in required 'agree to terms of use?' checkbox onto account-creation forms (wip -- all working except for validation error rendering)

--- opencore/themes/cleantheme/browser/views.py
+++ opencore/themes/cleantheme/browser/views.py
@@ -5,3 +5,22 @@ from opencore.nui.main.view import GetStarted as GetStartedBase
 class GetStarted(GetStartedBase):
     render = ViewPageTemplateFile('get-started.pt')
 
+class TermsOfUseViewlet(ViewletBase):
+
+    render = ViewPageTemplateFile('termsofuse.pt')
+
+    def validate(self):
+        self.errors = {}
+
+        view = self.__parent__
+        request = self.request
+
+        if not request.form.has_key('termsofuse'):
+            self.errors = {"termsofuse": "You must agree to the terms of use"}
+            return self.errors
+
+        if not request.form['termsofuse']:
+            self.errors = {"termsofuse": "You must agree to the terms of use"}
+            return self.errors
+
+        return self.errors
